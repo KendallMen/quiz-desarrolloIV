1d2665fef5c7ce20bedbd211a6b4b7a06800d27c
KendallMen <kendall.mendezcalderon@ucr.a.cr>
2026-09-16 15:49:39 -0600

    feat: bootstrap spring boot project

---
+++ src/main/java/com/quiz/c4h142/C4h142Application.java
@@ -0,0 +1,13 @@
+package com.quiz.c4h142;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C4h142Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C4h142Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/quiz/c4h142/C4h142ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiz.c4h142;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C4h142ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

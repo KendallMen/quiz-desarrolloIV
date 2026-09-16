1555a484c4c834252e6cfe4e879e9eedb6235404
KendallMen <kendall.mendezcalderon@ucr.a.cr>
2026-09-16 15:49:49 -0600

    feat: add cliente entity

---
+++ src/main/java/com/quiz/c4h142/data/Cliente.java
@@ -0,0 +1,68 @@
+package com.quiz.c4h142.data;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String cedula;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String correo;
+
+    public Cliente() {
+    }
+
+    public Cliente(Long id, String cedula, String nombre, String correo) {
+        this.id = id;
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+}
